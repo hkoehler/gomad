0bfff777bd18978ed1e62e4a849ed1191391e6bc
Heiko Koehler <heiko@pernixdata.com>
2016-09-17 13:09:30 -0700

    Forgot new config.go file ;)

---
+++ config.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+type Config struct {
+	Port     int // TCP port for HTTP service
+	Handlers []*HandlerConfig
+}
+
+var (
+	ConfigPath string
+	Port       int
+)
+
+// create HTTP handlers from config
+func LoadConfig(f *os.File) {
+	var config Config
+
+	dec := json.NewDecoder(f)
+	if err := dec.Decode(&config); err != nil {
+		log.Fatal(err)
+	}
+
+	if config.Port != 0 {
+		Port = config.Port
+		log.Printf("Port: %d\n", Port)
+	}
+	for _, handlerConf := range config.Handlers {
+		log.Println(handlerConf)
+		if handler, err := NewHandler(*handlerConf); err == nil {
+			RegisterHandler(handler)
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+
