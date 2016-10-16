3c351d7af05d33cab8591d0373d9a372e69bb56a
Heiko Koehler <mail@heikokoehler.de>
2016-10-15 20:21:58 -0700

    Added more CSS styling. Added header to every site by introducing master HTML template to which all HTTP handler must add their templates to used header.

--- handler.go
+++ handler.go
@@ -18,7 +18,10 @@ import (
 	"time"
 )
 
-var Registry = make(map[string]Handler)
+var (
+	Registry    = make(map[string]Handler)
+	masterTempl *template.Template
+)
 
 // Registry entry is HTTP handler with path
 type Handler interface {
@@ -71,12 +74,15 @@ type CommandHandler struct {
 	// map property name to regex and time series
 	Properties map[string]Property
 	Charts     []ChartConfig
+	Tmpl       *template.Template
 }
 
 // compile regular expression and create time series tables
 func NewCommandHandler(conf HandlerConfig) (Handler, error) {
 	var pollInterval time.Duration
 	var propMap = make(map[string]Property)
+	var tmpl *template.Template
+	var err error
 
 	for _, propConfig := range conf.Properties {
 		if re, err := regexp.Compile(propConfig.Regex); err != nil {
@@ -101,8 +107,45 @@ func NewCommandHandler(conf HandlerConfig) (Handler, error) {
 			return nil, err
 		}
 	}
+
+	const tmplStr = `
+		<!DOCTYPE html>
+		<html>
+			<head>
+			{{template "style"}}
+			<title> {{.Cmd}} </title>
+			</head>
+			<body>
+				{{template "header"}}
+				<h1 style="text-align:center"> {{.Cmd}} </h1>
+				<table style="width:100%;border:1px solid black">
+					<caption> {{.Cmd}} Output </caption>
+					<tr> 
+						<td text-align: left>
+						<code> {{ .FirstLine }} </code>
+						{{range .AdditionalLines}} <br> <code> {{.}} </code> {{end}}
+						</td>
+					</tr>
+				</table>
+				{{range .Charts}}
+				<h2 style="text-align:center"> {{.Name}} </h2>
+				<h3 style="text-align:center"> Last 5 minutes </h3>
+				<img src="{{.Path}}/2" alt="{{.Name}}" width="100%" style="border:1px solid black"> <br>
+				<h3 style="text-align:center"> Last 5 hours </h3>
+				<img src="{{.Path}}/1" alt="{{.Name}}" width="100%" style="border:1px solid black"> <br>
+				<h3 style="text-align:center"> Last 10 days </h3>
+				<img src="{{.Path}}/0" alt="{{.Name}}" width="100%" style="border:1px solid black"> <br>
+				{{end}}
+			</body>
+		</html>
+	`
+
+	if tmpl, err = masterTempl.New("command").Parse(tmplStr); err != nil {
+		log.Fatal(err)
+	}
+
 	return &CommandHandler{HandlerImpl: HandlerImpl{conf.URL, conf.Name, pollInterval},
-			CmdLine: conf.Cmd, Properties: propMap, Charts: conf.Charts},
+			CmdLine: conf.Cmd, Properties: propMap, Charts: conf.Charts, Tmpl: tmpl},
 		nil
 }
 
@@ -148,7 +191,7 @@ func (handler CommandHandler) Stat() (string, map[string]string) {
 // query properties and store them in time series logs
 func (handler CommandHandler) Execute() {
 	_, props := handler.Stat()
-	fmt.Println(props)
+	//fmt.Println(props)
 	for key, val := range props {
 		var floatVal float64
 
@@ -207,36 +250,6 @@ func (handler CommandHandler) ServeHTTP(w http.ResponseWriter, req *http.Request
 		}
 	}
 
-	const tmplStr = `
-		<!DOCTYPE html>
-		<html>
-			<head>
-			<title> {{.Cmd}} </title>
-			</head>
-			<body>
-				<h1 style="text-align:center"> {{.Cmd}} </h1>
-				<table border="line" style="width:100%">
-					<caption> {{.Cmd}} Output </caption>
-					<tr> 
-						<td text-align: left>
-						<code> {{ .FirstLine }} </code>
-						{{range .AdditionalLines}} <br> <code> {{.}} </code> {{end}}
-						</td>
-					</tr>
-				</table>
-				{{range .Charts}}
-				<h2 style="text-align:center"> {{.Name}} </h2>
-				<h3 style="text-align:center"> Last 5 minutes </h3>
-				<img src="{{.Path}}/2" alt="{{.Name}}" style="width:100%"> <br>
-				<h3 style="text-align:center"> Last 5 hours </h3>
-				<img src="{{.Path}}/1" alt="{{.Name}}" style="width:100%"> <br>
-				<h3 style="text-align:center"> Last 10 days </h3>
-				<img src="{{.Path}}/0" alt="{{.Name}}" style="width:100%"> <br>
-				{{end}}
-			</body>
-		</html>
-	`
-
 	out, _ := handler.Stat()
 	charts := make([]Chart, 0)
 	for _, chart := range handler.Charts {
@@ -250,9 +263,7 @@ func (handler CommandHandler) ServeHTTP(w http.ResponseWriter, req *http.Request
 	if len(lines) > 1 {
 		page.AdditionalLines = lines[1:]
 	}
-	if tmpl, err := template.New("command").Parse(tmplStr); err != nil {
-		log.Fatal(err)
-	} else if err := tmpl.Execute(w, page); err != nil {
+	if err := handler.Tmpl.Execute(w, page); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -261,21 +272,51 @@ func (handler CommandHandler) ServeHTTP(w http.ResponseWriter, req *http.Request
 type ConfigHandler struct {
 	HandlerImpl
 	ConfigPath string
+	Tmpl       *template.Template
 }
 
 func NewConfigHandler(path string, configPath string) Handler {
-	return &ConfigHandler{HandlerImpl: HandlerImpl{path, "Config", 0},
-		ConfigPath: configPath}
+	const tmplStr = `
+		<!DOCTYPE html>
+		<html>
+			<head>
+			{{template "style"}}
+			<title> Config </title>
+			</head>
+			<body>
+				{{template "header"}}
+				<h1 style="text-align:center"> Config </h1>
+				<iframe src="{{.}}" style="border:1px solid black" height=1000 width=100%>
+			</body>
+		</html>	`
+
+	if tmpl, err := masterTempl.New("config").Parse(tmplStr); err != nil {
+		log.Panic(fmt.Sprintf("Failed to parse HTML template: %v", err))
+		return nil
+	} else {
+		return &ConfigHandler{HandlerImpl: HandlerImpl{path, "Config", 0},
+			ConfigPath: configPath, Tmpl: tmpl}
+	}
 }
 
 func (handler ConfigHandler) Execute() {
 }
 
 func (handler ConfigHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if f, err := os.Open(handler.ConfigPath); err == nil {
-		io.Copy(w, f)
+	var txtPath = filepath.Join(handler.Path(), "text")
+
+	if req.URL.Path == txtPath {
+		if f, err := os.Open(handler.ConfigPath); err == nil {
+			if _, err := io.Copy(w, f); err != nil {
+				fmt.Println("Failed to read %s: %v", handler.ConfigPath, err)
+			}
+		} else {
+			fmt.Fprintf(w, "Couldn't open %s: %v", handler.ConfigPath, err)
+		}
 	} else {
-		fmt.Fprintf(w, "Couldn't open %s: %v", handler.Path(), err)
+		if err := handler.Tmpl.Execute(w, txtPath); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
@@ -339,6 +380,7 @@ type CPULoadHandler struct {
 	SystemTS *TimeSeriesTable
 	IdleTS   *TimeSeriesTable
 	Load     SystemLoad
+	Tmpl     *template.Template
 }
 
 func timeSeriesPath(url, prop string) string {
@@ -361,9 +403,32 @@ func NewCPULoadHandler() (Handler, error) {
 		return nil, err
 	}
 	pollInterval, _ := time.ParseDuration("1s")
-	return &CPULoadHandler{HandlerImpl: HandlerImpl{"/cpu", "CPU Load", pollInterval},
-		UserTS: userTS, SystemTS: systemTS, IdleTS: idleTS,
-		Load: NewSystemLoad()}, nil
+	const tmplStr = `
+		<!DOCTYPE html>
+		<html>
+			<head>
+			{{template "style"}}
+			<title> CPU Load </title>
+			</head>
+			<body>
+				{{template "header"}}
+				<h1 style="text-align:center"> CPU Load </h1>
+				<h2 style="text-align:center"> Last 5 Minutes </h2>
+				<img src="{{.}}/2" width="100%" style="border:1px solid black"> <br>
+				<h2 style="text-align:center"> Last 5 Hours </h2>
+				<img src="{{.}}/1" width="100%" style="border:1px solid black"> <br>
+				<h2 style="text-align:center"> Last 10 Days </h2>
+				<img src="{{.}}/0" width="100%" style="border:1px solid black"> <br>
+			</body>
+		</html>	`
+	if tmpl, err := masterTempl.New("cpu").Parse(tmplStr); err != nil {
+		log.Fatal(err)
+		return nil, err
+	} else {
+		return &CPULoadHandler{HandlerImpl: HandlerImpl{"/cpu", "CPU Load", pollInterval},
+			UserTS: userTS, SystemTS: systemTS, IdleTS: idleTS,
+			Load: NewSystemLoad(), Tmpl: tmpl}, nil
+	}
 }
 
 func (handler *CPULoadHandler) Execute() {
@@ -386,23 +451,6 @@ func (handler *CPULoadHandler) Execute() {
 }
 
 func (handler *CPULoadHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	const tmplStr = `
-		<!DOCTYPE html>
-		<html>
-			<head>
-			<title> CPU Load </title>
-			</head>
-			<body>
-				<h1 style="text-align:center"> CPU Load </h1>
-				<h2 style="text-align:center"> Last 5 Minutes </h2>
-				<img src="{{.}}/2" alt="Idle" style="width:100%"> <br>
-				<h2 style="text-align:center"> Last 5 Hours </h2>
-				<img src="{{.}}/1" alt="Idle" style="width:100%"> <br>
-				<h2 style="text-align:center"> Last 10 Days </h2>
-				<img src="{{.}}/0" alt="Idle" style="width:100%"> <br>
-			</body>
-		</html>	`
-
 	if relPath, err := filepath.Rel(handler.Path(), req.URL.Path); err == nil {
 		if relPath != "." {
 			var level int
@@ -419,19 +467,37 @@ func (handler *CPULoadHandler) ServeHTTP(w http.ResponseWriter, req *http.Reques
 		}
 	}
 
-	if tmpl, err := template.New("command").Parse(tmplStr); err != nil {
-		log.Fatal(err)
-	} else if err := tmpl.Execute(w, handler.Path()); err != nil {
+	if err := handler.Tmpl.Execute(w, handler.Path()); err != nil {
 		log.Fatal(err)
 	}
 }
 
 type RootHandler struct {
 	HandlerImpl
+	Tmpl *template.Template
 }
 
 func NewRootHandler() Handler {
-	return &RootHandler{HandlerImpl: HandlerImpl{"/", "Root", 0}}
+	const tmplStr = `
+		<html>
+			<head>
+			{{template "style"}}
+			<title> Registered Commands </title>
+			</head>
+			<body>
+				<h1> Registered Commands </h1>
+				<div> </div>
+				{{range .}} <a href="{{.Path}}"> {{.Name}} </a> <br>
+				{{end}}
+			</body>
+		</html>
+	`
+	if tmpl, err := masterTempl.New("index").Parse(tmplStr); err != nil {
+		log.Fatal(err)
+		return nil
+	} else {
+		return &RootHandler{HandlerImpl: HandlerImpl{"/", "Root", 0}, Tmpl: tmpl}
+	}
 }
 
 func (handler RootHandler) Execute() {
@@ -457,22 +523,31 @@ func (handler RootHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	sort.Sort(ByName(entries))
-	const tmplStr = `
-		<html>
-			<head>
-			<title> Registered Commands </title>
-			</head>
-			<body>
-				<h1> Registered Commands </h1>
-				<div> </div>
-				{{range .}} <a href="{{.Path}}"> {{.Name}} </a> <br>
-				{{end}}
-			</body>
-		</html>
-	`
-	if tmpl, err := template.New("index").Parse(tmplStr); err != nil {
-		log.Fatal(err)
-	} else if err := tmpl.Execute(w, entries); err != nil {
+	if err := handler.Tmpl.Execute(w, entries); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func init() {
+	const headerStr = `
+		<div style="background-color:powderblue; font-size:20px;border:20px solid powderblue">
+		<a href="/" style="padding: 21px 50px 21px 20px;"> Monitoring and Alerting Daemon </a>
+		<a href="http://opensource.org/licenses/MIT" style="float: right; padding-right: 20px; padding-left: 50px"> License </a>
+		<a href="http://www.github.com/hkoehler/gomad" style="float: right; padding-left: 50px"> Project Site </a>
+		</div>
+		<br>
+	`
+	
+	const styleStr = `
+		<style>
+		body 	{background-color: white;}
+		</style>
+	`
+
+	if templ, err := template.New("header").Parse(headerStr); err != nil {
+		log.Fatal(err)
+	} else {
+		masterTempl = templ
+	}
+	masterTempl.New("style").Parse(styleStr)
 }
